cd7f830934b5ae93ae1434ce0255d540a518902c
Gabriel Sosa <sosagabriel@gmail.com>
2013-05-13 12:30:13 -0400

    improved deps include. added verbose flag

--- Makefile
+++ Makefile
@@ -1,7 +1,9 @@
 FLAGS=-Wall
 TARGET=log-lines-counter
+INCLUDE=./lib
+
 build:
-	gcc $(FLAGS) log-lines-counter.c -o $(TARGET)
+	gcc $(FLAGS) -I$(INCLUDE) log-lines-counter.c -o $(TARGET)
 
 clean:
 	rm $(TARGET)
--- log-lines-counter.c
+++ log-lines-counter.c
@@ -1,24 +1,28 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
-#include "lib/statsd-client.c"
+#include <syslog.h>
+
+#include "statsd-client.c"
 
 #define DEFAULT_PORT 8125
 #define DEFAULT_HOST  "127.0.0.1"
 #define DEFAULT_SAMPLE_RATE 1.0
 
-#define VERSION "0.2"
+#define VERSION "0.3"
 
 
 void showHelp() {
     printf("log-lines-counter version %s\n", VERSION);
     printf("emits a hit to the statsd server for each line received\n\n");
     printf("Usage: cat log | log-lines-counter [OPTIONS]\n");
-    printf("-N <metric>\tmetric name to update (required)\n");
-    printf("-h <host>\tstatsd host (default: %s)\n", DEFAULT_HOST);
-    printf("-p <post>\tstatsd port (default: %d)\n", DEFAULT_PORT);
-    printf("-s <rate>\tsample rate (default: %2.2f)\n", DEFAULT_SAMPLE_RATE);
+    printf("\t-N <metric>         metric name to update (required)\n");
+    printf("\t-h <host>           statsd host (default: %s)\n", DEFAULT_HOST);
+    printf("\t-p <post>           statsd port (default: %d)\n", DEFAULT_PORT);
+    printf("\t-s <rate>           sample rate (default: %2.2f)\n", DEFAULT_SAMPLE_RATE);
+    printf("\t-v                  verbose mode\n");
 
 }
 
@@ -32,10 +36,10 @@ int main(int argc, char *argv[])
     float sample_rate = DEFAULT_SAMPLE_RATE;
     int port = DEFAULT_PORT;
     char *host = DEFAULT_HOST;
+    int verbose = 0;
 
 
-
-    while ((c = getopt (argc, argv, "h::p::s:N:")) != -1) {
+    while ((c = getopt (argc, argv, "vh::p::s:N:")) != -1) {
         switch (c) {
             case 'h':
                 host = optarg;
@@ -50,6 +54,9 @@ int main(int argc, char *argv[])
             case 's':
                 sample_rate = (float) atof(optarg);
             break;
+            case 'v':
+                verbose = 1;
+            break;            
             case '?':
                 showHelp();
                 return 0;
@@ -67,6 +74,14 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    if (verbose) {
+        setlogmask(LOG_UPTO(LOG_DEBUG));
+        openlog("log-lines-counter",  LOG_CONS | LOG_NDELAY | LOG_PERROR | LOG_PID, LOG_USER);
+        syslog(LOG_DEBUG, "statsd hosts: %s:%d", host, port);
+        syslog(LOG_DEBUG, "metric name: %s", metric_name);
+        syslog(LOG_DEBUG, "sample rate: %2.2f", sample_rate);
+    }
+
     char *line = NULL;
     size_t size;
 
@@ -74,8 +89,10 @@ int main(int argc, char *argv[])
 
     while (getline(&line, &size, stdin) != -1) {
         statsd_count(metric_name, 1, sample_rate);
+        syslog(LOG_DEBUG, "sent hit to statsd @ sample rate: %2.2f", sample_rate);
     }
 
+    syslog(LOG_DEBUG, "exiting...");
     statsd_finalize();
     return 0;
 }
